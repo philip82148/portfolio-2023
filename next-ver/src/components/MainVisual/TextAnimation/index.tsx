6966d9595e15e4e13c94eecc596c85114e3e12ec
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-07-10 03:05:40 +0900

    make durationS random

--- next-ver/src/components/MainVisual/TextAnimation/index.tsx
+++ next-ver/src/components/MainVisual/TextAnimation/index.tsx
@@ -1,20 +1,26 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useLayoutEffect, useState } from 'react'
 
 import { CharacterAnimation } from './CharacterAnimation'
 
 export const TextAnimation: React.FC<{ children: string }> = ({ children }) => {
-  const [state, setState] = useState(0)
+  const [durationSs, setDurationSs] = useState<number[]>([])
+
+  const randomInt = (min: number, max: number): number => Math.floor(Math.random() * (max - min + 1)) + min
+
+  useLayoutEffect(() => {
+    setDurationSs([...Array(children.length)].map(() => randomInt(5, 10)))
+  }, [children.length])
 
   useEffect(() => {
     setInterval(() => {
-      setState((state) => state + 1)
+      setDurationSs([...Array(children.length)].map(() => randomInt(5, 10)))
     }, 20000)
-  }, [setState])
+  }, [children.length])
 
   return (
     <>
       {children.split('').map((character, i) => (
-        <CharacterAnimation durationS={5} key={i} state={state}>
+        <CharacterAnimation durationS={durationSs[i] ?? 5} key={i} state={durationSs}>
           {character}
         </CharacterAnimation>
       ))}
