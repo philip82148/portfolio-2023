a839ed0629cddbefca688d66d90adf7e8a3a07ed
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-12-30 23:49:00 +0900

    improve styles of About for sp

--- next-ver/src/components/About.tsx
+++ next-ver/src/components/About.tsx
@@ -1,11 +1,15 @@
 import { Box, Container, Stack, Typography, styled } from '@mui/material'
 
-import { useIsPC } from '@/lib/useIsPC'
-
 const NameTypography = styled(Typography)(({ theme }) => ({
   fontSize: '2.8rem',
   fontWeight: 600,
   marginBottom: theme.spacing(0.5),
+  [theme.breakpoints.down('md')]: {
+    textAlign: 'center',
+  },
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '2rem',
+  },
 }))
 
 const ItemTypography = styled(Typography)(({ theme }) => ({
@@ -16,29 +20,36 @@ const ItemTypography = styled(Typography)(({ theme }) => ({
 
 const ContentTypography = styled(Typography)(() => ({
   fontSize: '1.2rem',
+  wordBreak: 'keep-all',
 }))
 
 export const About: React.FC = () => {
-  const isPC = useIsPC()
-
   return (
-    <Container fixed={isPC} sx={{ pt: 10, pb: 15 }} disableGutters={!isPC} id="about">
+    <Container fixed sx={{ pt: 10, pb: 15 }} id="about">
       <Stack sx={{ width: '100%' }} alignItems="center">
         <Typography variant="h2">ABOUT</Typography>
         <Stack
-          direction="row"
+          direction={{ md: 'row', xs: 'column' }}
           justifyContent="space-between"
           alignItems="center"
           sx={{ width: '100%', maxWidth: 950 }}
         >
-          <Box sx={{ width: 400, borderRadius: '50%', overflow: 'hidden', display: 'flex' }}>
+          <Box
+            sx={{
+              width: { lg: 400, sm: 350, xs: 220 },
+              borderRadius: '50%',
+              overflow: 'hidden',
+              display: 'flex',
+              mb: { md: 0, xs: 4 },
+            }}
+          >
             <img
               src="https://avatars.githubusercontent.com/u/92205270?v=4"
               alt=""
               style={{ width: '100%' }}
             />
           </Box>
-          <Stack sx={{ width: 400 }}>
+          <Stack sx={{ width: { md: 400 } }}>
             <NameTypography>Ryota Sasaki</NameTypography>
             <ItemTypography>所属</ItemTypography>
             <ContentTypography>慶應義塾大学 理工学部 電気情報工学科 4年</ContentTypography>
