2fa7eb64ec2f2a394984b507faef47bd27f2dd27
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-12-30 23:12:21 +0900

    improve styles of header & main visual for sp

--- next-ver/src/components/Header.tsx
+++ next-ver/src/components/Header.tsx
@@ -1,34 +1,43 @@
-import { Box, Button, Container, Stack, styled } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+import { Box, Button, Container, Drawer, IconButton, Stack, styled } from '@mui/material'
 import { Dosis } from 'next/font/google'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
-const h2Font = Dosis({ preload: false })
-// const mainFont = Crete_Round({ weight: '400', preload: false })
+import { useIsPC } from '@/lib/useIsPC'
 
-const HpButton = styled(Button)({
+const h2Font = Dosis({ preload: false })
+
+const HpButton = styled(Button)(({ theme }) => ({
   height: 60,
   borderRadius: 0,
   textTransform: 'none',
   fontWeight: 600,
   padding: '0 12px',
-})
+  [theme.breakpoints.down('md')]: {
+    padding: 0,
+  },
+}))
 
 const SnsButton = styled(Button)({
   height: 60,
+  width: 54,
   borderRadius: 0,
   minWidth: 0,
-  padding: '15px 12px',
+  padding: '15px 0',
 })
 
-const HeaderButton = styled(Button)({
+const HeaderButton = styled(Button)(({ theme }) => ({
   height: 60,
   borderRadius: 0,
   fontFamily: h2Font.style.fontFamily,
   fontWeight: 700,
   fontSize: '1rem',
   width: 80,
-})
+  [theme.breakpoints.down('md')]: {
+    width: '100%',
+  },
+}))
 
 export const Header: React.FC = () => {
   const [pr, setPr] = useState('0')
@@ -51,11 +60,15 @@ export const Header: React.FC = () => {
 
   const { basePath } = useRouter()
 
+  const [drawerOpened, setDrawerOpened] = useState(false)
+
+  const isPC = useIsPC()
+
+  const DrawerComponent = isPC ? Stack : Drawer
+
   return (
     <Box
       sx={{
-        display: 'flex',
-        alignItems: 'center',
         position: 'fixed',
         top: 0,
         left: 0,
@@ -67,20 +80,42 @@ export const Header: React.FC = () => {
     >
       <Container>
         <Stack direction="row" alignItems="center" justifyContent="space-between">
-          <Stack direction="row" alignItems="center">
-            <HpButton href={`${basePath}/`} color="inherit">
-              <Stack direction="row" alignItems="center">
-                <Box sx={{ width: 48, height: 48, borderRadius: 24, overflow: 'hidden', mr: 2 }}>
-                  <img
-                    src="https://avatars.githubusercontent.com/u/92205270?s=48&v=4"
-                    alt=""
-                    style={{ width: '100%' }}
-                  />
-                </Box>
-                Ryota Sasaki
-              </Stack>
-            </HpButton>
-            <Stack direction="row">
+          <HpButton href={`${basePath}/`} color="inherit">
+            <Stack direction="row" alignItems="center">
+              <Box sx={{ width: 48, height: 48, borderRadius: 24, overflow: 'hidden', mr: 2 }}>
+                <img
+                  src="https://avatars.githubusercontent.com/u/92205270?s=48&v=4"
+                  alt=""
+                  style={{ width: '100%' }}
+                />
+              </Box>
+              Ryota Sasaki
+            </Stack>
+          </HpButton>
+          <DrawerComponent
+            {...(isPC
+              ? {
+                  direction: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  sx: { flexGrow: 1 },
+                }
+              : {
+                  anchor: 'right',
+                  open: drawerOpened,
+                  onClose: () => {
+                    setDrawerOpened(false)
+                  },
+                  PaperProps: { sx: { p: 2, pt: 1 } },
+                })}
+          >
+            <Stack
+              direction="row"
+              flexWrap={isPC ? 'nowrap' : 'wrap'}
+              sx={{
+                width: 'min(calc(100vw - 100px), calc(54px * 6))',
+              }}
+            >
               <SnsButton href="https://github.com/philip82148" color="inherit">
                 <img
                   src="images/logos/github.svg"
@@ -112,33 +147,47 @@ export const Header: React.FC = () => {
                 />
               </SnsButton>
             </Stack>
-          </Stack>
-          <Stack direction="row" alignItems="center">
-            <HeaderButton
-              color="inherit"
-              onClick={() =>
-                document.getElementById('about')?.scrollIntoView({ behavior: 'smooth' })
-              }
+
+            <Stack direction={isPC ? 'row' : 'column'}>
+              <HeaderButton
+                color="inherit"
+                onClick={() => {
+                  document.getElementById('about')?.scrollIntoView({ behavior: 'smooth' })
+                  setDrawerOpened(false)
+                }}
+              >
+                ABOUT
+              </HeaderButton>
+              <HeaderButton
+                color="inherit"
+                onClick={() => {
+                  document.getElementById('skills')?.scrollIntoView({ behavior: 'smooth' })
+                  setDrawerOpened(false)
+                }}
+              >
+                SKILLS
+              </HeaderButton>
+              <HeaderButton
+                color="inherit"
+                onClick={() => {
+                  document.getElementById('history')?.scrollIntoView({ behavior: 'smooth' })
+                  setDrawerOpened(false)
+                }}
+              >
+                HISTORY
+              </HeaderButton>
+            </Stack>
+          </DrawerComponent>
+          {!isPC && (
+            <IconButton
+              onClick={() => {
+                setDrawerOpened(true)
+              }}
+              sx={{ pr: 0 }}
             >
-              ABOUT
-            </HeaderButton>
-            <HeaderButton
-              color="inherit"
-              onClick={() =>
-                document.getElementById('skills')?.scrollIntoView({ behavior: 'smooth' })
-              }
-            >
-              SKILLS
-            </HeaderButton>
-            <HeaderButton
-              color="inherit"
-              onClick={() =>
-                document.getElementById('history')?.scrollIntoView({ behavior: 'smooth' })
-              }
-            >
-              HISTORY
-            </HeaderButton>
-          </Stack>
+              <MenuIcon />
+            </IconButton>
+          )}
         </Stack>
       </Container>
     </Box>
--- next-ver/src/components/MainVisual.tsx
+++ next-ver/src/components/MainVisual.tsx
@@ -1,36 +1,45 @@
-import { Box, Stack } from '@mui/material'
+import { Box, Container, Stack } from '@mui/material'
 import { Crete_Round } from 'next/font/google'
+
+import { useIsPC } from '@/lib/useIsPC'
 
 const mainFont = Crete_Round({ weight: '400', preload: false })
 
 export const MainVisual: React.FC = () => {
+  const isPC = useIsPC()
+
   return (
-    <Stack
-      justifyContent="end"
-      alignItems="center"
-      className={mainFont.className}
-      sx={{
-        height: '550px',
-        width: '100%',
-        background: 'url(images/bg.jpg)',
-        backgroundSize: 'cover',
-        overflow: 'hidden',
-        mt: '60px',
-        color: '#fff',
-        pb: 12,
-      }}
-    >
-      <Box
+    <Container disableGutters={isPC} maxWidth={false}>
+      <Stack
+        justifyContent={{ sm: 'end', xs: 'center' }}
+        alignItems="center"
+        className={mainFont.className}
         sx={{
-          fontSize: { lg: 100, sm: 60, xs: 40 },
-          ml: { lg: '-6px', sm: '-4px', xs: '-2px' },
+          height: { md: 550, sm: 440, xs: 350 },
+          width: '100%',
+          background: 'url(images/bg.jpg)',
+          backgroundSize: 'cover',
+          overflow: 'hidden',
+          mt: '60px',
+          color: '#fff',
+          p: 1,
+          pb: { md: 12, sm: 11, xs: 0 },
+          textAlign: 'center',
         }}
       >
-        Ryota Sasaki
-      </Box>
-      <Box sx={{ fontSize: { lg: 26, md: 18, sm: 22 } }}>
-        Keio University Student Software Engineer
-      </Box>
-    </Stack>
+        <Box
+          sx={{
+            fontSize: { lg: 100, sm: 80, xs: 44 },
+            ml: { lg: '-6px', sm: '-2px', xs: '-2px' },
+          }}
+        >
+          Ryota Sasaki
+        </Box>
+        <Box sx={{ fontSize: { lg: 26, sm: 20, xs: 16 } }}>
+          <span style={{ display: 'inline-block' }}>Keio University Student</span>
+          <span style={{ display: 'inline-block' }}>Software Engineer</span>
+        </Box>
+      </Stack>
+    </Container>
   )
 }
