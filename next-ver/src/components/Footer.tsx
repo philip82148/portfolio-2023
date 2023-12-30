c402cea4e2e30c4750790bce8091a0a51a25bf40
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-12-30 18:17:44 +0900

    improve footer

--- next-ver/src/components/Footer.tsx
+++ next-ver/src/components/Footer.tsx
@@ -1,14 +1,11 @@
-import { Box, Button, Container, Stack } from '@mui/material'
+import { Box, Container, Stack, Typography } from '@mui/material'
 
 export const Footer: React.FC = () => {
   return (
     <Box sx={{ bgcolor: '#343434' }}>
       <Container>
-        <Stack direction="row" justifyContent="flex-end">
-          <Button>あ</Button>
-          <Button>あ</Button>
-          <Button>あ</Button>
-          <Button>あ</Button>
+        <Stack direction="row" alignItems="center" sx={{ height: 60 }}>
+          <Typography color="#fff">(C) 2023 Ryota Sasaki</Typography>
         </Stack>
       </Container>
     </Box>
