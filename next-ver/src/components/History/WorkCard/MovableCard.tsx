2905c1a80e35130224d6dffd057821bfc104b31a
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-01-02 11:41:54 +0900

    improve performance of WorkCardPC

--- next-ver/src/components/History/WorkCard/MovableCard.tsx
+++ next-ver/src/components/History/WorkCard/MovableCard.tsx
@@ -1,6 +1,6 @@
 import type { SxProps, Theme } from '@mui/material'
 import { Box } from '@mui/material'
-import { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 export const MovableCard: React.FC<
   React.PropsWithChildren<{
@@ -9,53 +9,46 @@ export const MovableCard: React.FC<
     innerSx?: SxProps<Theme>
   }>
 > = ({ align = 'left', children, outerSx, innerSx }) => {
-  const [marginLeft, setMarginLeft] = useState<number>()
+  const [mlForRightAlign, setMlForRightAlign] = useState<number>()
+  const [mlForCenterAlign, setMlForCenterAlign] = useState<number>()
 
   const parentBoxRef = useRef<HTMLDivElement>(null)
   const childBoxRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     const onResize = (): void => {
-      if (align === 'left') {
-        setMarginLeft(0)
-        return
-      }
-
       if (!parentBoxRef.current || !childBoxRef.current) return
 
       const { clientWidth: parentWidth } = parentBoxRef.current
       const { offsetWidth: childWidth } = childBoxRef.current
 
-      if (align === 'right') {
-        setMarginLeft(parentWidth - childWidth)
-      } else {
-        setMarginLeft(parentWidth / 2)
-      }
+      setMlForRightAlign(parentWidth - childWidth)
+      setMlForCenterAlign(parentWidth / 2)
     }
 
     onResize()
     window.addEventListener('resize', onResize)
-
     return () => {
       window.removeEventListener('resize', onResize)
     }
-  }, [align])
+  }, [])
+
+  const mlNum = { left: 0, 'center-start': mlForCenterAlign, right: mlForRightAlign }[align]
+  const ml = typeof mlNum === 'number' ? `${mlNum}px` : 'auto'
+  const mr = typeof mlNum === 'number' ? `-${mlNum}px` : 'auto'
 
   return (
-    <Box sx={{ width: '100%', ...outerSx }}>
-      <Box ref={parentBoxRef} sx={{ width: '100%', height: '100%' }}>
-        <Box
-          ref={childBoxRef}
-          sx={{
-            width: 'min-content',
-            transition: 'margin-left 1s',
-            ml: typeof marginLeft === 'number' ? `${marginLeft}px` : 'auto',
-            mr: typeof marginLeft === 'number' ? `-${marginLeft}px` : 'auto',
-            ...innerSx,
-          }}
-        >
-          {children}
-        </Box>
+    <Box ref={parentBoxRef} sx={{ width: '100%', ...outerSx }}>
+      <Box
+        ref={childBoxRef}
+        sx={{
+          transition: 'margin-left 1s',
+          ml,
+          mr,
+          ...innerSx,
+        }}
+      >
+        {children}
       </Box>
     </Box>
   )
--- next-ver/src/components/History/WorkCard/WorkCardPC.tsx
+++ next-ver/src/components/History/WorkCard/WorkCardPC.tsx
@@ -1,8 +1,9 @@
 import CloseIcon from '@mui/icons-material/Close'
 import { Box, Fade, IconButton, Link, Modal, Paper, Stack, Typography } from '@mui/material'
 import type { SyntheticEvent } from 'react'
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { memo, useCallback, useEffect, useRef, useState } from 'react'
 
+import type { TechType } from '../TechTag'
 import { TechTag } from '../TechTag'
 
 import { MovableCard } from './MovableCard'
@@ -135,40 +136,7 @@ export const WorkCardPC: React.FC<WorkCardProps> = ({
           pt: !isClosed ? 4 : 0,
         }}
       >
-        <Link
-          ref={titleRef}
-          onClick={onTitleClick}
-          href={url}
-          target="_blank"
-          underline="none"
-          sx={[
-            {
-              display: 'block',
-              position: 'relative',
-              zIndex: 1,
-              color: cardColor,
-              fontWeight: 700,
-              fontSize: '1.3rem',
-              width: 'fit-content',
-              transition: 'margin-left 1s, max-width 1s',
-              maxWidth: captionBoxWidth,
-            },
-            !!isClosed && {
-              cursor: 'pointer',
-              ml: -28,
-              maxWidth: { xs: '100vw' },
-            },
-            (!!url || !!isClosed) && {
-              '&:hover': {
-                mt: -0.5,
-                pb: 0.5,
-                opacity: 0.8,
-              },
-            },
-          ]}
-        >
-          {title}
-        </Link>
+        <TitleMovableCardChild {...{ titleRef, onTitleClick, url, cardColor, isClosed, title }} />
       </MovableCard>
       <MovableCard
         align={rightAlign ? 'right' : 'left'}
@@ -188,125 +156,203 @@ export const WorkCardPC: React.FC<WorkCardProps> = ({
                 outerSx={{ gridArea: '1 / 1 / 2 / 2' }}
                 innerSx={{ width: captionBoxWidth }}
               >
-                <Stack justifyContent="space-between" sx={{ p: 4, minHeight: 200 }}>
-                  <Stack>
-                    <Link
-                      underline="none"
-                      ref={dummyTitleRef}
-                      sx={{
-                        display: 'block',
-                        fontWeight: 700,
-                        fontSize: '1.3rem',
-                        visibility: 'hidden',
-                      }}
-                    >
-                      <span>{title}</span>
-                    </Link>
-                    {demoUrl && (
-                      <Link
-                        href={demoUrl}
-                        variant="body2"
-                        underline="none"
-                        target="_blank"
-                        sx={{
-                          zIndex: 1,
-                          width: 'fit-content',
-                          color: '#999',
-                          fontSize: '0.9rem',
-                          pt: 0.5,
-                          '&:hover': {
-                            mt: -0.5,
-                            pb: 0.5,
-                            opacity: 0.8,
-                          },
-                        }}
-                        onClick={onDemoClick}
-                      >
-                        {demoUrl}
-                      </Link>
-                    )}
-                    <Typography variant="body2" sx={{ pt: 1 }}>
-                      {caption}
-                    </Typography>
-                  </Stack>
-                  {techs && (
-                    <Stack direction="row" flexWrap="wrap" useFlexGap spacing={0.5} sx={{ pt: 2 }}>
-                      {techs.map((tag, i) => (
-                        <TechTag key={i} techType={tag} color={cardColor} />
-                      ))}
-                    </Stack>
-                  )}
-                </Stack>
+                <CaptionMovableCardChild
+                  {...{ dummyTitleRef, title, demoUrl, cardColor, onDemoClick, caption, techs }}
+                />
               </MovableCard>
               <MovableCard
                 align={rightAlign ? 'left' : 'right'}
                 outerSx={{ gridArea: '1 / 1 / 2 / 2' }}
-                innerSx={{
-                  display: 'grid',
-                  alignItems: 'center',
-                  justifyItems: 'center',
-                  width: imageBoxWidth,
-                  height: '100%',
-                }}
+                innerSx={{ width: imageBoxWidth, height: '100%' }}
               >
-                <Link
-                  href={demoUrl}
-                  target="_blank"
-                  sx={{
-                    position: 'relative',
-                    zIndex: 2,
-                    width: '100%',
-                    height: '100%',
-                    background: `url(${imageSrc})`,
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center top',
-                  }}
-                  onClick={openModal}
-                />
+                <ImageMovableCardChild {...{ demoUrl, imageSrc, openModal }} />
               </MovableCard>
             </Paper>
           </Box>
         </Fade>
       </MovableCard>
-      {!demoUrl && (
-        <Modal open={isModalOpen} onClose={closeModal}>
-          <Box
-            sx={{
-              display: 'flex',
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-              boxShadow: 24,
-              color: '#fff',
-            }}
-          >
-            <IconButton
-              onClick={closeModal}
-              color="inherit"
-              sx={{
-                position: 'absolute',
-                left: '100%',
-                bottom: '100%',
-                transform: 'translate(-20%, 20%)',
-              }}
-            >
-              <CloseIcon color="inherit" fontSize="large" />
-            </IconButton>
-            <img
-              src={imageSrc}
-              alt=""
-              style={{
-                maxHeight: '80vh',
-                maxWidth: '80vw',
-                height: '100%',
-                width: '100%',
-                objectFit: 'contain',
-              }}
-            />
-          </Box>
-        </Modal>
-      )}
+      {!demoUrl && <ImageModal {...{ isModalOpen, closeModal, imageSrc }} />}
     </Box>
   )
 }
+
+// パフォーマンス改善
+const TitleMovableCardChild = memo<{
+  titleRef: React.RefObject<HTMLAnchorElement>
+  onTitleClick: (e: SyntheticEvent<HTMLAnchorElement>) => void
+  url: string | undefined
+  cardColor: string
+  isClosed: boolean | undefined
+  title: string
+}>(({ titleRef, onTitleClick, url, cardColor, isClosed, title }) => {
+  return (
+    <Link
+      ref={titleRef}
+      onClick={onTitleClick}
+      href={url}
+      target="_blank"
+      underline="none"
+      sx={[
+        {
+          display: 'block',
+          position: 'relative',
+          zIndex: 1,
+          color: cardColor,
+          fontWeight: 700,
+          fontSize: '1.3rem',
+          width: 'fit-content',
+          transition: 'margin-left 1s, max-width 1s',
+          maxWidth: '100%',
+        },
+        !!isClosed && {
+          cursor: 'pointer',
+          ml: -28,
+          maxWidth: { xs: '100vw' },
+        },
+        (!!url || !!isClosed) && {
+          '&:hover': {
+            mt: -0.5,
+            pb: 0.5,
+            opacity: 0.8,
+          },
+        },
+      ]}
+    >
+      {title}
+    </Link>
+  )
+})
+TitleMovableCardChild.displayName = 'TitleMovableCardChild'
+
+const CaptionMovableCardChild = memo<{
+  dummyTitleRef: React.RefObject<HTMLAnchorElement>
+  title: string
+  demoUrl: string | undefined
+  cardColor: string
+  onDemoClick: (e: SyntheticEvent<HTMLAnchorElement>) => void
+  caption: React.ReactNode
+  techs: TechType[] | undefined
+}>(({ dummyTitleRef, title, demoUrl, cardColor, onDemoClick, caption, techs }) => {
+  return (
+    <Stack justifyContent="space-between" sx={{ p: 4, minHeight: 200 }}>
+      <Stack>
+        <Link
+          underline="none"
+          ref={dummyTitleRef}
+          sx={{
+            display: 'block',
+            fontWeight: 700,
+            fontSize: '1.3rem',
+            visibility: 'hidden',
+          }}
+        >
+          <span>{title}</span>
+        </Link>
+        {demoUrl && (
+          <Link
+            href={demoUrl}
+            variant="body2"
+            underline="none"
+            target="_blank"
+            sx={{
+              zIndex: 1,
+              width: 'fit-content',
+              color: '#999',
+              fontSize: '0.9rem',
+              pt: 0.5,
+              '&:hover': {
+                mt: -0.5,
+                pb: 0.5,
+                opacity: 0.8,
+              },
+            }}
+            onClick={onDemoClick}
+          >
+            {demoUrl}
+          </Link>
+        )}
+        <Typography variant="body2" sx={{ pt: 1 }}>
+          {caption}
+        </Typography>
+      </Stack>
+      {techs && (
+        <Stack direction="row" flexWrap="wrap" useFlexGap spacing={0.5} sx={{ pt: 2 }}>
+          {techs.map((tag, i) => (
+            <TechTag key={i} techType={tag} color={cardColor} />
+          ))}
+        </Stack>
+      )}
+    </Stack>
+  )
+})
+CaptionMovableCardChild.displayName = 'CaptionMovableCardChild'
+
+const ImageMovableCardChild = memo<{
+  demoUrl: string | undefined
+  imageSrc: string
+  openModal: (e: SyntheticEvent<HTMLAnchorElement>) => void
+}>(({ demoUrl, imageSrc, openModal }) => {
+  return (
+    <Link
+      href={demoUrl}
+      target="_blank"
+      sx={{
+        display: 'block',
+        position: 'relative',
+        zIndex: 2,
+        width: '100%',
+        height: '100%',
+        background: `url(${imageSrc})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center top',
+      }}
+      onClick={openModal}
+    />
+  )
+})
+ImageMovableCardChild.displayName = 'ImageMovableCardChild'
+
+const ImageModal = memo<{ isModalOpen: boolean; closeModal: () => void; imageSrc: string }>(
+  ({ isModalOpen, closeModal, imageSrc }) => {
+    return (
+      <Modal open={isModalOpen} onClose={closeModal}>
+        <Box
+          sx={{
+            display: 'flex',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            boxShadow: 24,
+            color: '#fff',
+          }}
+        >
+          <IconButton
+            onClick={closeModal}
+            color="inherit"
+            sx={{
+              position: 'absolute',
+              left: '100%',
+              bottom: '100%',
+              transform: 'translate(-20%, 20%)',
+            }}
+          >
+            <CloseIcon color="inherit" fontSize="large" />
+          </IconButton>
+          <img
+            src={imageSrc}
+            alt=""
+            style={{
+              maxHeight: '80vh',
+              maxWidth: '80vw',
+              height: '100%',
+              width: '100%',
+              objectFit: 'contain',
+            }}
+          />
+        </Box>
+      </Modal>
+    )
+  },
+)
+ImageModal.displayName = 'ImageModal'
