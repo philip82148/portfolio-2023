9a132522538804f1ac304b8736699526441acd17
philip82148 <92205270+philip82148@users.noreply.github.com>
2024-02-25 14:43:10 +0900

    add GitHub Stats section

--- next-ver/src/components/Skills.tsx
+++ next-ver/src/components/Skills.tsx
@@ -4,41 +4,61 @@ export const Skills: React.FC = () => {
   return (
     <Stack
       sx={{ bgcolor: 'primary.main', pt: { lg: 8, md: 5, xs: 3 }, pb: { lg: 8, md: 5, xs: 3 } }}
+      spacing={{ lg: 8, md: 5, xs: 3 }}
       alignItems="center"
     >
-      {/* <Typography variant="h2" color="#fff" borderColor="#fff">
-        SKILLS
-      </Typography> */}
       <Container fixed>
         <Stack
-          sx={{
-            width: '100%',
-            bgcolor: 'background.default',
-            pb: { lg: 10, md: 7, xs: 6 },
-            pl: { lg: 16, md: 14, sm: 8, xs: 4 },
-            pr: { lg: 16, md: 14, sm: 8, xs: 4 },
-          }}
+          sx={{ bgcolor: 'background.default', pb: { lg: 10, md: 7, xs: 6 } }}
           alignItems="center"
         >
-          {/* <Typography variant="h3">LANGUAGES</Typography> */}
           <Typography variant="h2" sx={{ mt: { lg: 0, md: -3, xs: -4 } }} id="skills">
             SKILLS
           </Typography>
-          {/* <Box sx={{ width: 500 }}>
+          <Stack
+            sx={{
+              maxWidth: { md: 500, sm: 420, xs: 400 },
+              width: '100%',
+              pl: { sm: 0, xs: 2 },
+              pr: { sm: 0, xs: 2 },
+            }}
+          >
             <img
-              src="https://skillicons.dev/icons?i=ts,js,cpp,c,java,py,php,html,css,mysql&perline=5"
-              alt=""
+              src="https://skillicons.dev/icons?i=ts,js,nextjs,react,nestjs,prisma,nodejs,docker,cpp,c,java,py,php,jquery,html,css,flutter,kotlin,graphql,mysql,wordpress,gcp,firebase,raspberrypi&perline=6"
+              alt="Skill Icons"
               style={{ width: '100%' }}
             />
-          </Box>
+          </Stack>
         </Stack>
-        <Stack sx={{ width: '100%', bgcolor: 'background.default' }} alignItems="center">
-          <Typography variant="h3">FRAMEWORKS</Typography> */}
-          <img
-            src="https://skillicons.dev/icons?i=ts,js,nextjs,react,nestjs,prisma,nodejs,docker,cpp,c,java,py,php,jquery,html,css,flutter,kotlin,graphql,mysql,wordpress,gcp,firebase,raspberrypi&perline=6"
-            alt=""
-            style={{ width: '100%' }}
-          />
+      </Container>
+      <Container fixed>
+        <Stack
+          sx={{ bgcolor: 'background.default', pb: { lg: 10, md: 7, xs: 6 } }}
+          alignItems="center"
+        >
+          <Typography variant="h2" sx={{ mt: { lg: 0, md: -3, xs: -4 } }}>
+            GITHUB STATS
+          </Typography>
+          <Stack
+            sx={{
+              maxWidth: { md: 500, sm: 420, xs: 400 },
+              width: '100%',
+              pl: { sm: 0, xs: 2 },
+              pr: { sm: 0, xs: 2 },
+            }}
+            spacing={2}
+          >
+            <img
+              src="https://github-readme-stats-sooty-nine-75.vercel.app/api?username=philip82148&show_icons=true&exclude_repo=github-readme-stats,qutip-tutorials-ipynb,selva,kishukusha-hp-theme,testrepo"
+              alt="GitHub Stats"
+              style={{ width: '100%' }}
+            />
+            <img
+              src="https://github-readme-stats-sooty-nine-75.vercel.app/api/top-langs/?username=philip82148&layout=donut&exclude_repo=github-readme-stats,qutip-tutorials-ipynb,selva,kishukusha-hp-theme,testrepo"
+              alt="Top Langs"
+              style={{ width: '100%' }}
+            />
+          </Stack>
         </Stack>
       </Container>
     </Stack>
