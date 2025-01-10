5ce58305a0124ff1b33e08b841c0eab632a2c07e
philip82148 <92205270+philip82148@users.noreply.github.com>
2025-01-10 10:54:22 +0900

    add modal to suggest 2024

--- next-ver/src/pages/index.tsx
+++ next-ver/src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { Box, Button, Modal, Stack, Typography } from '@mui/material'
 import type { NextPage } from 'next'
+import { useState } from 'react'
 
 import { About } from '@/components/About'
 import { Footer } from '@/components/Footer'
@@ -9,8 +11,45 @@ import { MainVisual } from '@/components/MainVisual'
 import { Skills } from '@/components/Skills'
 
 const Home: NextPage = () => {
+  const [modalOpen, setModalOpen] = useState(true)
+  const handleClose = (): void => {
+    setModalOpen(false)
+  }
+
   return (
     <>
+      <Modal
+        open={modalOpen}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 500,
+            bgcolor: 'background.paper',
+            p: 4,
+          }}
+        >
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            2024年版の新しいポートフォリオがあります。
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            2024年版の新しいポートフォリオを開きますか？
+          </Typography>
+          <Stack direction="row" justifyContent="space-between" sx={{ mt: 4 }}>
+            <Button variant="contained" href="https://philip82148.dev/">
+              2024年版を見る
+            </Button>
+            <Button variant="contained" color="inherit" onClick={handleClose}>
+              ダイアログを閉じて2023年版を見る
+            </Button>
+          </Stack>
+        </Box>
+      </Modal>
       <Header />
       <MainVisual />
       <About />
